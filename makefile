668379bafd333839190b763c2eba669dba2ffbac
Ivan Rabcevskij <chupchushka@Ivans-MacBook-Air.local>
2025-09-05 12:15:18 +0200

    feature: added a posibility to erase a tag based on id of task

--- makefile
+++ makefile
@@ -2,13 +2,16 @@ all:
 	clang++ -o clist src/main.cpp -lsqlite3
 	sudo ln -s /Users/chupchushka/ToDo-list/clist /usr/local/bin/clist
 	echo "Build complete."
+
 clean:
 	sudo rm /usr/local/bin/clist
 	rm -f clist
 	rm -f db.sqlite3
 	echo "Clean complete."
+
 rebuild: clean all
 	echo "Rebuild complete."
+
 test: rebuild
 	clist -a "Test task 1"
 	clist -a "Test task 2"
--- src/todo_service.hpp
+++ src/todo_service.hpp
@@ -2,49 +2,65 @@
 #include <iostream>
 #include "db_service.hpp"
 
-class TodoService {
-    private:
-        DbService &dbService;
-    public:
-        TodoService(DbService &db) : dbService(db) {
-        }
+class TodoService
+{
+private:
+    DbService &dbService;
 
-        void addTask(const char* task) {
-            char sql[256];
-            snprintf(sql, sizeof(sql), "INSERT INTO tasks (task, done) VALUES ('%s', 0);", task);
-            dbService.execSQL(sql);
-        }
+public:
+    TodoService(DbService &db) : dbService(db)
+    {
+    }
 
-        void removeTask(const int id) {
-            char sql[256];
-            snprintf(sql, sizeof(sql), "DELETE FROM tasks WHERE id = ('%d')", id);
-            dbService.execSQL(sql);
-        }
+    void addTask(const char *task)
+    {
+        char sql[256];
+        snprintf(sql, sizeof(sql), "INSERT INTO tasks (task, done) VALUES ('%s', 0);", task);
+        dbService.execSQL(sql);
+    }
 
-        void listTasks() {
-            dbService.readDataStmt("tasks");
-        }
+    void removeTask(const int id)
+    {
+        char sql[256];
+        snprintf(sql, sizeof(sql), "DELETE FROM tasks WHERE id = ('%d')", id);
+        dbService.execSQL(sql);
+    }
 
-        void markTaskDone(const int id, bool done) {
-            char sql[256];
-            snprintf(sql, sizeof(sql), "UPDATE tasks SET done = %d WHERE id = %d;", done ? 1 : 0, id);
-            dbService.execSQL(sql);
-        }
-        
-        void addTag(const char* tag, int taskId) {
-            char sql[256];
-            snprintf(sql, sizeof(sql), "UPDATE tasks SET tags = '%s' WHERE id = %d;", tag, taskId);
-            dbService.execSQL(sql);
-        }
+    void listTasks()
+    {
+        dbService.readDataStmt("tasks");
+    }
 
-        void print_help() {
-            std::cout << "Clist CLI Help\n";
-            std::cout << "Usage:\n";
-            std::cout << "  add <task>            or -a <task>            : Add a new task\n";
-            std::cout << "  remove <id>           or -r <id>              : Remove a task by id\n";
-            std::cout << "  list                  or -l                   : List all tasks\n";
-            std::cout << "  mark <id> done/false  or -m <id> done/false   : Mark task as done/false\n";
-            std::cout << "  help                  or -h                   : Show this help message\n";
-        }
-    
+    void markTaskDone(const int id, bool done)
+    {
+        char sql[256];
+        snprintf(sql, sizeof(sql), "UPDATE tasks SET done = %d WHERE id = %d;", done ? 1 : 0, id);
+        dbService.execSQL(sql);
+    }
+
+    void addTag(const char *tag, int taskId)
+    {
+        char sql[256];
+        snprintf(sql, sizeof(sql), "UPDATE tasks SET tags = '%s' WHERE id = %d;", tag, taskId);
+        dbService.execSQL(sql);
+    }
+    void removeTag(const int id)
+    {
+        char sql[256];
+        snprintf(sql, sizeof(sql), "UPDATE tasks SET tags = '' WHERE id = %d;", id);
+        dbService.execSQL(sql);
+    }
+
+    void print_help()
+    {
+        std::cout << "Clist CLI Help\n";
+        std::cout << "Usage:\n";
+        std::cout << "  add <task>            or -a <task>              : Add a new task\n";
+        std::cout << "  remove <id>           or -r <id>                : Remove a task by id\n";
+        std::cout << "  list                  or -l                     : List all tasks\n";
+        std::cout << "  mark <id> done/false  or -m <id> done/false     : Mark task as done/false\n";
+        std::cout << "  tag <tag> <id>        or -t <tag> <id>          : Add a tag to a task\n";
+        std::cout << "  rtag <id>             or -rt <id>               : Remove tag from a task\n";
+        std::cout << "  help                  or -h                     : Show this help message\n";
+    }
 };
